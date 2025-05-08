ae7c4cffbfba9b5cdf1308fd655c563eb366ed41
Jacob Zwang <59858341+JacobZwang@users.noreply.github.com>
2025-05-08 09:22:45 -0700

    clean up template

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,12 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import freestyleLogo from "/placeholder-freestyle-logo.svg";
 import "./App.css";
 
 function App() {
   return (
     <div className="flex items-center justify-center flex-col min-h-screen bg-gray-100">
       <a href="https://vite.dev" target="_blank">
-        <img src={viteLogo} className="logo" alt="Vite logo" />
+        <img src={freestyleLogo} alt="Vite logo" className="opacity-10 w-48" />
       </a>
-      <a href="https://react.dev" target="_blank">
-        <img src={reactLogo} className="logo react" alt="React logo" />
-      </a>
-
-      <h1>Vite + React + Tailwind</h1>
     </div>
   );
 }
